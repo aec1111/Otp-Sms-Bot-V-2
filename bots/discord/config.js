54431957e21306c156be463a5fd72b3126fa0a96
CoreLine76 <92768020+CoreLine76@users.noreply.github.com>
2022-10-07 07:26:43 +0300

    add bot

---
+++ bots/discord/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiurl: '',
+    apipassword: '',
+
+    discordtoken: '',
+    discordprefix: '.',
+    secretpassword: '',
+    botuser_rolename: 'Bot User',
+    admin_rolename: 'Admin'
+};
